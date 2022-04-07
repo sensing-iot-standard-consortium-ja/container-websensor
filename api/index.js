f23758426abe57cab1ea0846704767fc33d72a11
seiketkm <seike.takumi.999@tis.co.jp>
2022-04-07 10:07:34 +0900

    keyを一旦除去

--- api/index.js
+++ api/index.js
@@ -16,9 +16,9 @@ app.get("/health", (req, res) => {
 
 app.post("/:topic/container", (req, res) => {
     const topic = req.params.topic
-    const km = new KeyedMessage("container", req.body)
+    const messages = [req.body]
     const payloads = [
-        { topic: topic, messages: km }
+        { topic, messages }
     ]
     producer.send(payloads,(err) => {
         if (err)
@@ -29,9 +29,9 @@ app.post("/:topic/container", (req, res) => {
 
 app.post("/:topic/json", (req, res) => {
     const topic = req.params.topic
-    const km = new KeyedMessage("json", req.body)
+    const messages = [req.body]
     const payloads = [
-        { topic: topic, messages: km }
+        { topic, messages }
     ]
     producer.send(payloads,(err) => {
         if (err)
